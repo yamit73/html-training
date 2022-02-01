7fbe8257a2d6b94bbada95588c77e8a53469b528
Amit Yadav <amityadav@cedcoss.com>
2022-02-01 11:29:46 +0530

    subtask 9

---
+++ html/Add Images to Your Website.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>CatPhotoApp</h2>
+<main>
+
+
+  <p>Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.</p>
+  <p>Purr jump eat the grass rip the couch scratched sunbathe, shed everywhere rip the couch sleep in the sink fluffy fur catnip scratched.</p>
+  <img src="https://www.freecatphotoapp.com/your-image.jpg" alt="A business cat wearing a necktie.">
+</main>
+</body>
+</html>
